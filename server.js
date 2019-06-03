5efae7b5852b13834c96de88211cc0894b630621
chrispetito <christopherpetito@gmail.com>
2019-06-03 13:53:24 -0700

    add working CRUD requests sans error handling

--- server.js
+++ server.js
@@ -16,8 +16,23 @@ server.get("/api/accounts", (req, res) => {
       );
   });
 
+  server.get('/api/accounts/:id', (req, res) => {
+      const { id } = req.params;
+      db.findById(id).then(account => res.status(201).json(account))
+  })
+
   server.post("/api/accounts", (req, res) => {
-    db.add(req.body).then(response => res.status(201).json({ message: 'successful post'}))
+    db.add(req.body).then(response => res.status(201).json({ message: 'Account added succesfully!'}))
   });
+
+  server.delete('/api/accounts/:id', (req, res) => {
+      const { id } = req.params;
+      db.remove(id).then(response => res.status(201).json({ message: 'Account successfully deleted.'}))
+  })
+
+  server.put('/api/accounts/:id', (req, res) => {
+      const { id } = req.params;
+      db.update(id, req.body).then(rersponse => res.status(201).json({ message: 'Account successfully updated!'}))
+  })
 
 module.exports = server;
