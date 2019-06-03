6133c174c02d8fb7e917b4e601ce85fa02efc0cd
chrispetito <christopherpetito@gmail.com>
2019-06-03 15:00:35 -0700

    add 500 status to CRUD requests

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const db = require('./data/accounts-model')
+const express = require("express");
+const db = require("./data/accounts-model");
 
 const server = express();
 
@@ -7,32 +7,57 @@ const server = express();
 
 server.use(express.json());
 
-
 server.get("/api/accounts", (req, res) => {
-    db.find()
-      .then(accounts => res.status(201).json(accounts))
+  db.find()
+    .then(accounts => res.status(201).json(accounts))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: "Error getting users posts" })
+        .catch(err =>
+          res.status(500).json({ message: "Could not load accounts" })
+        )
+    );
+});
+
+server.get("/api/accounts/:id", (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(account => res.status(201).json(account))
+    .catch(err => res.status(500).json({ message: "Could not load account." }));
+});
+
+server.post("/api/accounts", (req, res) => {
+  db.add(req.body).then(response =>
+    res
+      .status(201)
+      .json({ message: "Account added succesfully!" })
+      .catch(err => res.status(500).json({ message: "Could not add account." }))
+  );
+});
+
+server.delete("/api/accounts/:id", (req, res) => {
+  const { id } = req.params;
+  db.remove(id).then(response =>
+    res
+      .status(201)
+      .json({ message: "Account successfully deleted." })
       .catch(err =>
-        res.status(500).json({ message: "Error getting users posts" })
-      );
-  });
+        res.status(500).json({ message: "Could not delete account." })
+      )
+  );
+});
 
-  server.get('/api/accounts/:id', (req, res) => {
-      const { id } = req.params;
-      db.findById(id).then(account => res.status(201).json(account))
-  })
-
-  server.post("/api/accounts", (req, res) => {
-    db.add(req.body).then(response => res.status(201).json({ message: 'Account added succesfully!'}))
-  });
-
-  server.delete('/api/accounts/:id', (req, res) => {
-      const { id } = req.params;
-      db.remove(id).then(response => res.status(201).json({ message: 'Account successfully deleted.'}))
-  })
-
-  server.put('/api/accounts/:id', (req, res) => {
-      const { id } = req.params;
-      db.update(id, req.body).then(rersponse => res.status(201).json({ message: 'Account successfully updated!'}))
-  })
+server.put("/api/accounts/:id", (req, res) => {
+  const { id } = req.params;
+  db.update(id, req.body).then(rersponse =>
+    res
+      .status(201)
+      .json({ message: "Account successfully updated!" })
+      .catch(err =>
+        res.status(500).json({ message: "Could not modify account." })
+      )
+  );
+});
 
 module.exports = server;
