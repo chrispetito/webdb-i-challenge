4624d805a50cd3e13f7b072fd38344fc0e276377
chrispetito <christopherpetito@gmail.com>
2019-06-03 13:26:45 -0700

    add db file, get and post requests

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const db = require('./data/accounts-model')
 
 const server = express();
 
 // your code here
+
+server.use(express.json());
+
+
+server.get("/api/accounts", (req, res) => {
+    db.find()
+      .then(accounts => res.status(201).json(accounts))
+      .catch(err =>
+        res.status(500).json({ message: "Error getting users posts" })
+      );
+  });
+
+  server.post("/api/accounts", (req, res) => {
+    db.add(req.body).then(response => res.status(201).json({ message: 'successful post'}))
+  });
 
 module.exports = server;
